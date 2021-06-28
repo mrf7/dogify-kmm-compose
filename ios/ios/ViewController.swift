5943d8e89bd71a8f7578a920f00bb7755385e141
Michael Friend <michaelf@touchlab.co>
2021-06-28 15:05:36 -0400

    add ios old school

--- ios/ios/ViewController.swift
+++ ios/ios/ViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import shared
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
